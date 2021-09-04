542d3a2fa765c8a9eb500ca7a74885b3909b8eb7
Khaimov <khaimov@reksoft.ru>
2021-09-04 17:24:08 +0300

    Handlers solution

--- src/func.ts
+++ src/func.ts
@@ -1,5 +1,8 @@
+import assert from 'assert';
+
 type Expression = {};
 
+// --- Literal ---
 type Literal = Expression & {
   value: number;
 };
@@ -8,6 +11,13 @@ function createLiteral(n: number): Literal {
   return { value: n };
 }
 
+function isLiteral(expression: Expression): expression is Literal {
+  return 'value' in expression;
+}
+
+// --- Literal ---
+
+// --- BinaryAdd ---
 type BinaryAdd = Expression & {
   left: Expression;
   right: Expression;
@@ -17,20 +27,47 @@ function createBinaryAdd(left: Expression, right: Expression): BinaryAdd {
   return { left, right };
 }
 
-function evaluate(expression: Expression | BinaryAdd | Literal): number {
-  if ('value' in expression) {
-    return expression.value;
-  }
-
-  if ('left' in expression) {
-    return evaluate(expression.left) + evaluate(expression.right);
-  }
-
-  return 0;
+function isBinaryAdd(expression: Expression): expression is BinaryAdd {
+  return 'left' in expression;
 }
+
+// --- BinaryAdd ---
+
+interface Handler<TReturn, TExpression> {
+  handles(expression: TExpression): boolean;
+
+  handle(
+    expression: TExpression,
+    visit: (expression: Expression) => TReturn
+  ): TReturn;
+}
+
+function apply<TReturn>(
+  expression: Expression,
+  handlers: Array<Handler<TReturn, Expression>>
+): TReturn {
+  const handler = handlers.find((handler) => handler.handles(expression));
+
+  assert(handler !== undefined);
+
+  return handler.handle(expression, (next) => apply(next, handlers));
+}
+
+type EvaluateHandler<TExpression> = Handler<number, TExpression>;
+
+const evaluateLiteral: EvaluateHandler<Literal> = {
+  handles: isLiteral,
+  handle: (expression) => expression.value,
+};
+
+const evaluateBinaryAdd: EvaluateHandler<BinaryAdd> = {
+  handles: isBinaryAdd,
+  handle: (expression, visit) =>
+    visit(expression.left) + visit(expression.right),
+};
 
 function makeOnePlusTwo() {
   return createBinaryAdd(createLiteral(1), createLiteral(2));
 }
 
-console.log(evaluate(makeOnePlusTwo()));
+console.log(apply(makeOnePlusTwo(), [evaluateLiteral, evaluateBinaryAdd]));
