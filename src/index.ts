226fa4463dcd3610082a5be9d8690fd46fb17f7a
Khaimov <khaimov@reksoft.ru>
2022-05-22 12:46:04 +0300

    1

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,33 @@
-const divide = (n: number, factor: number): number => {
-  if (factor === 0) {
-    throw new Error('Can not divide by zero');
-  }
-
-  return n / factor;
+type Right<TValue> = {
+  tag: 'right';
+  value: TValue;
 };
 
-const head = <T>(ns: T[]): T => {
+type Left<TValue> = {
+  tag: 'left';
+  value: TValue;
+};
+
+function left<TValue>(value: TValue): Left<TValue> {
+  return {
+    tag: 'left',
+    value,
+  };
+}
+
+function right<TValue>(value: TValue): Right<TValue> {
+  return {
+    tag: 'right',
+    value,
+  };
+}
+
+type Either<TLeft, TRight> = Left<TLeft> | Right<TRight>;
+
+const head = <T>(ns: T[]): Either<string, T> => {
   if (ns.length === 0) {
-    throw new Error('List is empty');
+    return left('List is empty');
   }
 
-  return ns[0];
+  return right(ns[0]);
 };
-
-const parse = (n: string): number => {
-  if (isNaN(parseInt(n, 10))) {
-    throw new Error('Not valid integer');
-  }
-
-  return parseInt(n, 10);
-};
-
-const input0 = ['10', '2', '1'];
-const input1 = '12';
-
-const firstN = head(input0);
-const secondN = parse(input1);
-
-const parsedN = parse(firstN);
-
-const result = divide(secondN, parsedN);
