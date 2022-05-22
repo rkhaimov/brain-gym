080185542a64b92982f0a88220dc765dc60e00af
Khaimov <khaimov@reksoft.ru>
2022-05-22 12:54:15 +0300

    2

--- src/index.ts
+++ src/index.ts
@@ -31,3 +31,32 @@ const head = <T>(ns: T[]): Either<string, T> => {
 
   return right(ns[0]);
 };
+
+const fold = <TLeft, TRight, TResult>(
+  either: Either<TLeft, TRight>,
+  onLeft: (left: TLeft) => TResult,
+  onRight: (left: TRight) => TResult
+) => {
+  if (either.tag === 'left') {
+    return onLeft(either.value);
+  }
+
+  return onRight(either.value);
+};
+
+const map = <TLeft, TRightA, TRightB, TResult>(
+  either: Either<TLeft, TRightA>,
+  transform: (value: TRightA) => TRightB
+): Either<TLeft, TRightB> => {
+  if (either.tag === 'left') {
+    return either;
+  }
+
+  return right(transform(either.value));
+};
+
+const headShout = (input: string[]) => {
+  return map(head(input), (str) => str.toUpperCase());
+};
+
+console.log(headShout([]));
