88d93c827a49b8ce223e5867b2965422af5a4d67
Khaimov <khaimov@reksoft.ru>
2022-09-09 10:38:55 +0300

    Impl web cube

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,124 @@
-import { BehaviorSubject, map, repeat, timer, withLatestFrom } from 'rxjs';
-import { vector, Vector } from './linalg';
+import * as tf from '@tensorflow/tfjs';
+import { concat, Tensor } from '@tensorflow/tfjs';
+import { Tensor2D } from '@tensorflow/tfjs-core/dist/tensor';
+import {
+  animationFrameScheduler,
+  BehaviorSubject,
+  concatMap,
+  map,
+  observeOn,
+  timer,
+  withLatestFrom,
+} from 'rxjs';
 
-console.log(1);
+const { renderPoint, clearCanvas } = createRenderers();
 
-const state$ = new BehaviorSubject<Vector[]>([
-  vector([0, 0]),
-  vector([0, -1]),
-  vector([0, -2]),
-  vector([0, -3]),
-]);
+const points$ = new BehaviorSubject<Tensor>(cube(16));
 
-timer(0, 5_000)
+timer(0, 1_000 / 60)
   .pipe(
-    withLatestFrom(state$),
-    map(([, state]) => {
-      const GRID_WIDTH = 100;
-      const GRID_HEIGHT = 50;
-
-      const cx = 50;
-      const cy = 25;
-      return grid(GRID_WIDTH, GRID_HEIGHT, (_x, _y) => {
-        const point = state.find(
-          (v) => v[0][0] === _x - cx && v[1][0] === cy - _y
-        );
-
-        if (point) {
-          return 'A';
-        }
-
-        return '.';
-      });
-    })
+    observeOn(animationFrameScheduler),
+    withLatestFrom(points$),
+    map(([, points]) => points),
+    map((points) => rotateYZ(Math.PI / 24).matMul(points)),
+    map((points) => rotateXY(Math.PI / 24).matMul(points))
   )
-  .subscribe((canvas) => {
-    console.clear();
+  .subscribe(points$);
 
-    console.log(canvas);
-  });
+points$
+  .pipe(
+    map((tensors) => scale(40).matMul(tensors)),
+    concatMap(renderTensors)
+  )
+  .subscribe();
 
-function grid(
-  width: number,
-  height: number,
-  draw: (x: number, y: number) => string
-): string {
-  return new Array(height)
+function rotateXY(degree: number): Tensor2D {
+  return tf.tensor2d([
+    [Math.cos(degree), -Math.sin(degree), 0],
+    [Math.sin(degree), Math.cos(degree), 0],
+    [0, 0, 1],
+  ]);
+}
+
+function rotateYZ(degree: number): Tensor2D {
+  return tf.tensor2d([
+    [1, 0, 0],
+    [0, Math.cos(degree), -Math.sin(degree)],
+    [0, Math.sin(degree), Math.cos(degree)],
+  ]);
+}
+
+function scale(scale: number): Tensor2D {
+  return tf.tensor2d([
+    [scale, 0, 0],
+    [0, scale, 0],
+    [0, 0, scale],
+  ]);
+}
+
+function cube(size: number): Tensor2D {
+  const grid: Tensor2D[] = new Array(size)
     .fill(0)
-    .map((_, y) =>
-      new Array(width)
+    .flatMap((_, x) =>
+      new Array(size)
         .fill(0)
-        .map((_, x) => draw(x, y))
-        .join('')
-    )
-    .join('\n');
+        .flatMap((_, y) =>
+          new Array(size)
+            .fill(0)
+            .map((_, z) =>
+              tf.tensor2d([x - size / 2, y - size / 2, z - size / 2], [3, 1])
+            )
+        )
+    );
+
+  return concat(grid, 1);
+}
+
+function renderTensors(tf: Tensor) {
+  return tf
+    .transpose()
+    .array()
+    .then((points) => {
+      clearCanvas();
+
+      return (points as number[][]).map(([x, y, z]) => renderPoint(x, y, z));
+    });
+}
+
+function createRenderers() {
+  document.querySelector('html')!.style.height = '100%';
+  document.body.style.height = '100%';
+  document.body.style.margin = '0';
+
+  document.body.innerHTML = `<svg id='canvas' xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'></svg>`;
+
+  const canvas = document.querySelector('#canvas') as HTMLElement;
+
+  return {
+    renderPoint,
+    clearCanvas: () => (canvas.innerHTML = ''),
+  };
+
+  function renderPoint(x: number, y: number, z: number, color = '#000') {
+    const circle = document.createElementNS(
+      'http://www.w3.org/2000/svg',
+      'circle'
+    );
+
+    circle.setAttribute('fill', color);
+
+    circle.setAttribute('cx', `${toWebX(x)}px`);
+    circle.setAttribute('cy', `${toWebY(y)}px`);
+    circle.setAttribute('r', `${10 * (Math.abs(z + 200) / 400)}px`);
+
+    canvas.appendChild(circle);
+  }
+
+  function toWebX(x: number) {
+    return x + window.innerWidth / 2;
+  }
+
+  function toWebY(y: number) {
+    return window.innerHeight / 2 - y;
+  }
 }
