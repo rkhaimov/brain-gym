60ac14fdbc50e523de9e2db9ee1c9e6b6636bb06
Khaimov <khaimov@reksoft.ru>
2022-09-13 15:45:18 +0300

    Plolty

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+///<reference path="module.ts"/>
+
 import * as tf from '@tensorflow/tfjs';
 import { Tensor } from '@tensorflow/tfjs';
+import Plotly from 'plotly.js-dist';
 import * as math from 'mathjs';
-
-const { renderPoint, clearCanvas } = createRenderers();
 
 void main();
 
@@ -17,8 +18,25 @@ async function main() {
   const Yt = tf.tensor(Y).reshape([Y.length, 1]);
 
   const theta = await learn(Xt, Yt);
+  const [[b], [k]] = theta.arraySync() as number[][];
 
-  theta.print();
+  document.body.innerHTML = '<div id="charts"></div>';
+  Plotly.newPlot('charts', [
+    {
+      x: X,
+      y: Y,
+      type: 'scatter',
+    },
+    {
+      x: tf.linspace(0, 10, 10).arraySync(),
+      y: tf
+        .linspace(0, 10, 10)
+        .arraySync()
+        .map((x) => b + x * k),
+      type: 'scatter',
+    },
+  ]);
+  // theta.print();
 }
 
 async function learn(X: Tensor, Y: Tensor): Promise<Tensor> {
@@ -32,44 +50,6 @@ async function learn(X: Tensor, Y: Tensor): Promise<Tensor> {
 
 function predict(X: Tensor, theta: Tensor): Tensor {
   return X.transpose().matMul(theta).sum();
-}
-
-function createRenderers() {
-  document.querySelector('html')!.style.height = '100%';
-  document.body.style.height = '100%';
-  document.body.style.margin = '0';
-
-  document.body.innerHTML = `<svg id='canvas' xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'></svg>`;
-
-  const canvas = document.querySelector('#canvas') as HTMLElement;
-
-  return {
-    renderPoint,
-    clearCanvas: () => (canvas.innerHTML = ''),
-  };
-
-  function renderPoint(x: number, y: number, color = '#000') {
-    const circle = document.createElementNS(
-      'http://www.w3.org/2000/svg',
-      'circle'
-    );
-
-    circle.setAttribute('fill', color);
-
-    circle.setAttribute('cx', `${toWebX(x)}px`);
-    circle.setAttribute('cy', `${toWebY(y)}px`);
-    circle.setAttribute('r', `${5}px`);
-
-    canvas.appendChild(circle);
-  }
-
-  function toWebX(x: number) {
-    return x + window.innerWidth / 2;
-  }
-
-  function toWebY(y: number) {
-    return window.innerHeight / 2 - y;
-  }
 }
 
 function times(n: number): number[] {
---
+++ src/module.ts
@@ -0,0 +1,5 @@
+declare module 'plotly.js-dist' {
+  const pl: any;
+
+  export default pl;
+}
