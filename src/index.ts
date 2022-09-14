460a826190b44d83079bb52e41528c025a7c061a
Khaimov <khaimov@reksoft.ru>
2022-09-14 15:43:43 +0300

    Log reg

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import * as tf from '@tensorflow/tfjs';
 import { Tensor } from '@tensorflow/tfjs';
 ///<reference path="module.ts"/>
 import Plotly from 'plotly.js-dist';
@@ -8,19 +7,20 @@ document.body.innerHTML = '<div id="charts"></div>';
 void main();
 
 async function main() {
-  const data = classification();
+  Plotly.newPlot('charts', []);
+}
 
-  const X = data.slice([0, 0], [-1, 2]);
-  const Y = data.slice([0, 2], [-1, 1]);
+function lines(tensor: Tensor, color: string) {
+  const list = tensor.arraySync() as number[][];
 
-  const left = await tf.booleanMaskAsync(data, Y.toBool().reshape([-1]));
-
-  const right = await tf.booleanMaskAsync(
-    data,
-    Y.toBool().logicalNot().reshape([-1])
-  );
-
-  Plotly.newPlot('charts', [markers(left, 'red'), markers(right, 'blue')]);
+  return {
+    x: list.map(([x]) => x),
+    y: list.map(([, y]) => y),
+    mode: 'line',
+    line: {
+      color,
+    },
+  };
 }
 
 function markers(tensor: Tensor, color: string) {
@@ -38,14 +38,4 @@ function markers(tensor: Tensor, color: string) {
 
 function times(n: number): number[] {
   return new Array(n).fill(0).map((_, index) => index);
-}
-
-function classification() {
-  const total = 25;
-
-  const coords = times(total).flatMap((x) =>
-    times(total).map((y) => [x, y, x + y < 25 ? 1 : 0])
-  );
-
-  return tf.tensor2d(coords);
 }
--- src/linreg.ts
+++ src/linreg.ts
@@ -3,14 +3,18 @@ import { Tensor } from '@tensorflow/tfjs';
 import * as math from 'mathjs';
 
 export async function learn(X: Tensor, Y: Tensor): Promise<Tensor> {
-  return X.transpose()
-    .matMul(X)
+  const Xb = tf.ones([X.shape[0], 1]).concat(X, 1);
+
+  return Xb.transpose()
+    .matMul(Xb)
     .array()
     .then((matrix) => math.inv(matrix as number[][]) as number[][])
-    .then((inverse) => tf.tensor(inverse).matMul(X.transpose()))
+    .then((inverse) => tf.tensor(inverse).matMul(Xb.transpose()))
     .then((pseudo) => pseudo.matMul(Y));
 }
 
-function predict(X: Tensor, theta: Tensor): Tensor {
-  return X.transpose().matMul(theta).sum();
+export function predict(X: Tensor, theta: Tensor): Tensor {
+  const Xb = tf.ones([X.shape[0], 1]).concat(X, 1);
+
+  return Xb.transpose().matMul(theta).sum();
 }
