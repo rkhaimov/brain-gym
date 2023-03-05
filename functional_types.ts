19bd92e71a5f95269e557166f9d025b2daf1768d
Khaimov <khaimov@reksoft.ru>
2023-03-05 15:55:44 +0500

    Mapped and Template types

--- functional_types.ts
+++ functional_types.ts
@@ -1,129 +1,75 @@
-type Apple = {
-  sweetness: number;
-  color: string;
+type UnboxArray<T> = T extends Array<infer R> ? R : never;
+
+declare const t0: UnboxArray<number[][]>
+
+type UnboxArrayDeep<T extends Array<unknown>> = T extends Array<infer R>
+  ?
+  R extends Array<unknown>
+    ? UnboxArrayDeep<R>
+    : R
+  : never;
+
+declare const t1: UnboxArrayDeep<number[][][][][][][]>
+
+function flatten<T extends Array<unknown>>(list: T): UnboxArrayDeep<T> {
+  return list.flatMap((it) => Array.isArray(it) ? flatten(it) : it) as UnboxArrayDeep<T>;
+}
+
+const t2 = flatten([1, 2, 3, [5, 6, [7]]]);
+
+type F = ReturnType<() => number>
+
+function toValueOrNone<T extends Record<string, unknown>>(collection: T) {
+  const entries = Object.entries(collection).map(([key, value]) => [key, value === undefined ? 'none' : value]);
+
+  return Object.fromEntries(entries);
+}
+
+type User = {
+  name: string;
+  surname: string | undefined;
+  age: number;
 };
 
-// Duplication
-function createApple(sweetness: number) {
-  return {sweetness, color: 'blue'};
-}
+type ToValueOrNone0<T extends Record<string, unknown>> = T;
 
-declare const t0: typeof createApple;
+declare const t3: ToValueOrNone0<User>;
 
-type Apple0 = ReturnType<typeof createApple>;
+type ToValueOrNone1<T extends Record<string, unknown>> = keyof T;
 
-function createRedApple(sweetness: number) {
-  return {sweetness, color: 'red' as const};
-}
+declare const t4: ToValueOrNone1<User>;
 
-function createBlueApple(sweetness: number) {
-  return {sweetness, color: 'blue' as const};
-}
+declare const t5: User['age' | 'name'];
 
-type RedApple = ReturnType<typeof createRedApple>;
-type BlueApple = ReturnType<typeof createBlueApple>;
+declare const t6: User[keyof User];
 
-type GardenApple = RedApple | BlueApple;
+type ToValueOrNone2<T extends Record<string, unknown>> = {
+  [TKey in keyof T]: undefined extends T[TKey] ? Exclude<T[TKey], undefined> | 'none' : T[TKey];
+};
 
-declare const t2: GardenApple;
+declare const t7: ToValueOrNone2<User>
 
-function infectWithWorm(apple: GardenApple) {
-  return {...apple, worm: 'Jimmy'};
-}
+type MyOmit<TObject, TKeys extends keyof TObject> = {
+  [TKey in Exclude<keyof TObject, TKeys>]: TObject[TKey];
+};
 
-infectWithWorm(createRedApple(10));
+declare const t8: MyOmit<{ name: string, surname: number }, 'surname'>;
 
-type InfectedApple = ReturnType<typeof infectWithWorm>;
+t8;
 
-function nopeWhenRed(color: GardenApple['color']) {
-  if (color === 'red') {
-    return 'NOPE';
-  }
+type ConvertOnHandlers0<S extends string> = Uppercase<S>;
 
-  return color;
-}
+declare const t9: ConvertOnHandlers0<'name' | 'surname' | 'age'>;
 
-function nopeWhenRed0<T extends string>(color: T) {
-  if (color === 'red') {
-    return 'NOPE';
-  }
+type ConvertOnHandlers1<S extends string> = S extends `on${infer RHandler}` ? Lowercase<RHandler> : S;
 
-  return color as Exclude<T, 'red'>;
-}
+declare const t10: ConvertOnHandlers1<'name' | 'surname' | 'onClick'>;
 
-declare const t3: 'red' | 'blue' | 'green' | 'purple'
+type RouteParamsFromTemplate<T extends string> = T extends `${infer RStart}:${infer RParam}/${infer REnd}`
+  ? { [TKey in RParam]: string } & RouteParamsFromTemplate<`${RStart}${REnd}`>
+  : {};
 
-nopeWhenRed0(t3)
-
-declare const t4: Exclude<1 | 2 | 3 | 4, unknown>;
-declare const t5: Exclude<{ name: string } | { surname: string }, { name?: string }>;
-
-type Identity<T> = T;
-
-declare const t6: Identity<1 | 2 | 3>;
-
-type OnlyNumbers0<T> = T extends number ? T : never;
-
-declare const t7: OnlyNumbers0<1 | 2 | 3 | '222'>;
-
-type OnlyNumbers1<T> = T extends number ? T : never;
-
-declare const t8: OnlyNumbers1<1 | 'string'>;
-
-// Expand
-declare const t9: 1 | never;
-
-// Try to implement differently
-type MyExtract<TCandidate, TPattern> = TCandidate extends TPattern ? TCandidate : never;
-type MyExclude<TCandidate, TPattern> = TCandidate extends TPattern ? never : TCandidate;
-
-declare const t10: MyExclude<1 | 2 | 3, 2 | 3 | 10>;
-
-function toOnlyRedApples0(apples: Array<GardenApple>): Array<RedApple> {
-  const result: Array<RedApple> = [];
-
-  for (const apple of apples) {
-    if (isRedApple(apple)) {
-      result.push(apple);
-    }
-  }
-
-  return result;
-}
-
-function toOnlyRedApples1(apples: Array<GardenApple>): Array<RedApple> {
-  return apples.filter(isRedApple);
-}
-
-function isRedApple(apple: GardenApple): apple is RedApple {
-  return apple.color === 'red';
-}
-
-function hang(): never {
-  return hang();
-}
-
-function ensureIsRedApple(apple: GardenApple) {
-  if (isRedApple(apple)) {
-    return apple;
-  }
-
-  return hang();
-}
-
-declare const t11: GardenApple;
-
-const redApple = ensureIsRedApple(t11);
-
-assertIsRedApple(t11)
-
-t11;
-
-function assertIsRedApple(apple: GardenApple): asserts apple is RedApple {
-  if (!isRedApple(apple)) {
-    throw new Error('Not Red!!!')
-  }
-}
+declare const t11: RouteParamsFromTemplate<'/user/:userId/article/:articleName/'>;
 
 // number string
 // brands using classes and tags
