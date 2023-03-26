94e52c60522e81f4e4ae5a17771b56ba0b27ca26
Khaimov <khaimov@reksoft.ru>
2023-03-26 16:40:03 +0500

    3_struct_disjoint

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,24 @@ import { World } from './World';
 import { Controls } from './Controls';
 import { PedestrianTrafficLight } from './PedestrianTrafficLight';
 
-type LightsState = 'STAND' | 'WALK';
+type LightsState = { type: 'stand' } | { type: 'walk' };
 
 export const App = () => {
-  const [state, setState] = useState<LightsState>('STAND');
+  const [state, setState] = useState<LightsState>({ type: 'stand' });
 
   return (
     <World>
       <Controls>
-        <Controls.Control onClick={() => setState('STAND')}>
+        <Controls.Control onClick={() => setState({ type: 'stand' })}>
           STANDING
         </Controls.Control>
-        <Controls.Control onClick={() => setState('WALK')}>
+        <Controls.Control onClick={() => setState({ type: 'walk' })}>
           WALKING
         </Controls.Control>
       </Controls>
       <PedestrianTrafficLight
-        standing={state === 'STAND'}
-        walking={state === 'WALK'}
+        standing={state.type === 'stand'}
+        walking={state.type === 'walk'}
       />
     </World>
   );
