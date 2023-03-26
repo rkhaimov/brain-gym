345f5c98e72513a85ab2b33713c1d7ee914f47e9
Khaimov <khaimov@reksoft.ru>
2023-03-26 16:22:06 +0500

    1_bool_fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 import { World } from './World';
 import { Controls } from './Controls';
 import { PedestrianTrafficLight } from './PedestrianTrafficLight';
+import { not } from './not';
 
 export const App = () => {
   const [standing, setStanding] = useState(false);
-  const [walking, setWalking] = useState(false);
 
   return (
     <World>
@@ -13,11 +13,11 @@ export const App = () => {
         <Controls.Control onClick={() => setStanding(true)}>
           STANDING
         </Controls.Control>
-        <Controls.Control onClick={() => setWalking(true)}>
+        <Controls.Control onClick={() => setStanding(false)}>
           WALKING
         </Controls.Control>
       </Controls>
-      <PedestrianTrafficLight standing={standing} walking={walking} />
+      <PedestrianTrafficLight standing={standing} walking={not(standing)} />
     </World>
   );
 };
