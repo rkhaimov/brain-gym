6b331c618a9b02151b79e81a395d7503148965d4
khaimov <khaimov@reksoft.ru>
2023-04-06 13:00:19 +0300

    4_extendable

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,15 @@
 main();
 
 async function main() {
-  console.log(head([]));
-  console.log(head([{ type: 'left', value: undefined }]));
+
 }
 
-// T | Error != a + 1 (Might have intersection)
-// Either<void, T> = a + 1;
-function head<T>(elements: T[]): Either<void, T> {
-  if (elements.length === 0) {
+function divide(n: number, by: number): Either<void, number> {
+  if (by === 0) {
     return { type: 'left', value: undefined };
   }
 
-  return { type: 'right', value: elements[0] };
+  return { type: 'right', value: n / by };
 }
 
 type Either<TLeft, TRight> =
