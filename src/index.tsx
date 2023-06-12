41d2df78f88375d6a0e607b331f07920d53d2618
Khaimov <khaimov@reksoft.ru>
2023-06-12 10:25:26 +0300

    LSP

--- src/index.tsx
+++ src/index.tsx
@@ -1,45 +1,11 @@
-declare function program(events: Events, queries: Queries): [Images, Commands];
+interface IRepository {
+  getAllAdministrators(max: number): Promise<NonEmptyArray<Admin>>;
 
-function testProgram() {
-  const [image, commands] = program(
-    events
-      .inputText('Логин', 'Админ')
-      .inputText('Пароль', 'admin')
-      .check('Запомнить')
-      .click('Войти'),
-    queries
-      .getAllUsers([{ name: 'Vasiliy' }, { name: 'Fedor' }])
-      .isAllowedToEnter(true)
-  );
-
-  expect(image).toMatchImageSnapshot();
-
-  // Снимок экрана и его сравнение с прошлой версией (при наличии)
-
-  expect(commands).toMatchCommandsSnapshot();
-
-  // login: [{ login: 'Админ', password: 'admin', remember: true }]
+  deleteAdministratorById(id: Admin['id']): Promise<void>;
 }
 
-declare function expect(input: unknown): {
-  toMatchImageSnapshot(): unknown;
-  toMatchCommandsSnapshot(): unknown;
+type Admin = {
+  id: number;
 };
 
-type Queries = {
-  getAllUsers(a: unknown): Queries;
-  isAllowedToEnter(a: unknown): Queries;
-};
-
-declare const queries: Queries;
-
-type Events = {
-  inputText(a: string, b: string): Events;
-  check(a: string): Events;
-  click(a: string): Events;
-};
-
-declare const events: Events;
-
-type Images = {};
-type Commands = {};
+type NonEmptyArray<T> = {};
