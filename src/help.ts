8e59684e4ab4ee98f1cb9f6685bda84da030eea7
Khaimov <khaimov@reksoft.ru>
2023-07-04 08:21:29 +0300

    Generics and Constraints

--- src/help.ts
+++ src/help.ts
@@ -1,66 +1,58 @@
-class User {
-  constructor(public name: string, public age: number) {}
+const identity0 = (input: unknown): unknown => input;
 
-  say(this: User) {
-    this;
-  }
+const identity1 = <T>(input: T) => input;
+
+identity1(1);
+
+const identityOnNumber = identity1<number>;
+
+const identity2 = <T>(input: T, un: unknown) => {
+  input = un;
+
+  return input;
+};
+
+const identity3 = <A, B>(left: A, right: B, l: keyof A) => {
+  right = left;
+};
+
+const createBox = <T>(initial: T) => {
+  let current = initial;
+
+  return {
+    get: () => current,
+    set: (next: T) => (current = next),
+  };
+};
+
+const nBox = createBox(10);
+
+const shoutSquaredAndReturn = <T extends number>(input: T) => {
+  input;
+
+  console.log('Received ', Math.pow(input, 2));
+
+  return input;
+};
+
+const aa = <A, B extends A>(left: A, right: B) => {
+  return right;
+};
+
+type AA = typeof aa;
+
+function b(input: AA) {
+  input(10, 20);
 }
 
-console.log(User);
-
-console.log();
-
-function act(user: typeof User) {
-  new user('awd', 12);
+function t0<T>(obj: T, prop: keyof T) {
+  return obj[prop];
 }
 
-declare const n: ConstructorParameters<typeof User>;
+const t = t0({ name: 'Vasiliy', age: 12 }, 'age');
 
-n.sort();
-
-n[0];
-n[1];
-n[2];
-
-const t: 2 = n.length;
-
-type Pair = [number, string];
-
-type Another = [boolean, ...Pair];
-
-declare const tt: Pair;
-
-declare const q: keyof Pair;
-
-if (q === '2') {
-  tt['0'];
+function t1<T, K extends keyof T>(obj: T, prop: K) {
+  return obj[prop];
 }
 
-declare const n0: [number, string, number] & [number, string, { b: number }];
-
-n0[3];
-
-const left = [1, 2] as const;
-const right = [3, 4, 5] as const;
-
-declare function act12(): ['wallet', number, string] | ['cash', boolean];
-
-const rr = act12();
-
-function act13(left: [number, string], right: [boolean]) {
-  const [t, q] = left;
-
-  return [left.slice(1), ...right] as const;
-}
-
-declare const t1: ReturnType<typeof act13>;
-
-t1.length;
-
-if (rr.length === 2) {
-  rr;
-}
-
-if (rr['0'] === 'wallet') {
-  rr;
-}
+const tt = t1({ name: 'Vasiliy', age: 12 }, 'age');
