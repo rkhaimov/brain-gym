c2cadcf2efe7647a167e58603a47b94bb4fd18e1
Khaimov <khaimov@reksoft.ru>
2023-07-12 08:23:55 +0300

    Sub principle

--- src/help.ts
+++ src/help.ts
@@ -1,58 +1,123 @@
-const identity0 = (input: unknown): unknown => input;
+class Shape {
+  getDescription(preface: string) {
+    assert(preface.length <= 5);
 
-const identity1 = <T>(input: T) => input;
+    const result = {
+      name: 'I am shape',
+      description: `${preface}: it is all about shapes`,
+    };
 
-identity1(1);
+    assert(result.name.length <= 20);
 
-const identityOnNumber = identity1<number>;
+    return result;
+  }
+}
 
-const identity2 = <T>(input: T, un: unknown) => {
-  input = un;
+class Rectangle extends Shape {
+  getDescription(preface: string) {
+    assert(preface.length <= 10);
 
+    return {
+      name: 'I am rectangle',
+      type: 'rectangle',
+      description: 'No description',
+    }; // narrower to wider or covariant
+  }
+}
+
+// Covariant
+const r: Shape = new Rectangle();
+
+const shoutShapeName = (input: Shape) => {
+  const meta = input.getDescription('Intro');
+
+  assert(meta.name.length <= 20);
+
+  console.log(meta.name);
+  console.log(meta.description);
+};
+
+shoutShapeName(new Rectangle());
+
+function assert(condition: boolean) {
+  if (!condition) {
+    throw new Error('Fails');
+  }
+}
+
+function sum(left: number, right: number): number {
+  assert(left >= 0);
+
+  const result = left + right;
+
+  assert(result - left === right);
+  assert(result - right === left);
+
+  return result;
+}
+
+const t: number = -1;
+
+console.log(sum(t, t));
+
+// SelectProps Contravariant on T
+type SelectProps0<in T> = {
+  onChange(option: T): void;
+};
+
+function act0(
+  input: SelectProps0<{ name: string }>
+): SelectProps0<{ name: string; surname: string }> {
   return input;
+}
+
+const result0 = act0({
+  onChange(option) {
+    option.name;
+  },
+});
+
+// SelectProps Covariant on T
+type SelectProps1<out T> = {
+  get(index: number): T;
+  length: number;
 };
 
-const identity3 = <A, B>(left: A, right: B, l: keyof A) => {
-  right = left;
-};
-
-const createBox = <T>(initial: T) => {
-  let current = initial;
-
-  return {
-    get: () => current,
-    set: (next: T) => (current = next),
-  };
-};
-
-const nBox = createBox(10);
-
-const shoutSquaredAndReturn = <T extends number>(input: T) => {
-  input;
-
-  console.log('Received ', Math.pow(input, 2));
-
+function act1(input: SelectProps1<{ name: string }>): SelectProps1<{}> {
   return input;
+}
+
+const result1 = act1({
+  get: (index: number) => ({ name: 'Vasiliy' }),
+  length: 1,
+});
+
+result1.get(0);
+
+// SelectProps is invariant on T
+type SelectProps<in out T> = {
+  get(index: number): T;
+  length: number;
+  onChange(option: T): void;
 };
 
-const aa = <A, B extends A>(left: A, right: B) => {
-  return right;
-};
-
-type AA = typeof aa;
-
-function b(input: AA) {
-  input(10, 20);
+function act2(
+  input: SelectProps<{ name: string }>
+): SelectProps<{ name: string }> {
+  return input;
 }
 
-function t0<T>(obj: T, prop: keyof T) {
-  return obj[prop];
+// Array covariant on T
+function act3(input: Array<{ name: string }>): Array<{}> {
+  return input;
 }
 
-const t = t0({ name: 'Vasiliy', age: 12 }, 'age');
+const names = [{ name: 'Vasiliy' }];
 
-function t1<T, K extends keyof T>(obj: T, prop: K) {
-  return obj[prop];
-}
+const result2 = act3(names);
 
-const tt = t1({ name: 'Vasiliy', age: 12 }, 'age');
+result2.push(false);
+
+console.log(names);
+
+export {};
