c5fab2c8f5debe5ccce5b0502695e96005c2823b
Khaimov <khaimov@reksoft.ru>
2023-12-09 15:44:07 +0300

    Part 3.1

--- src/index.tsx
+++ src/index.tsx
@@ -1,108 +1,66 @@
-function double(n: number) {
-  return n * 3;
-}
+const Expression = {
+  literal: (n: number) => ({ eval: () => n }),
+};
 
-/**
- * Представляет собой процесс тестирования любой чистой функции
- * @param expected Ожидаемое поведение функции ввиде таблицы ввода/вывода
- * @param program Тестируемое поведение
- */
-function test<K, V>(expected: Map<K, V>, program: (input: K) => V) {
-  const actual = map(expected, ([key]) => program(key));
+function Calculator() {
+  const input = useUserInput();
 
-  assert(equals(expected, actual));
-}
-
-function equals(
-  left: Map<unknown, unknown>,
-  right: Map<unknown, unknown>
-): boolean {
   return (
-    JSON.stringify(Array.from(left.values())) ===
-    JSON.stringify(Array.from(right.values()))
+    <>
+      <Output />
+      <NumPad />
+      <Equals
+        onClick={() => {
+          const { left, right, operation } = parse(input);
+
+          if (operation === 'sum') {
+            // Разработчик ошибся и передал left дважды
+            return sum(Expression.literal(left), Expression.literal(left));
+            //                            ^^^^                      ^^^^
+          }
+
+          //<editor-fold desc="Другие детали">
+          console.log(right);
+          //</editor-fold>
+        }}
+      />
+    </>
   );
 }
 
-function map<K, A, B>(
-  table: Map<K, A>,
-  transform: (kv: [K, A]) => B
-): Map<K, B> {
-  return new Map(
-    Array.from(table.entries()).map(([key, value]) => [
-      key,
-      transform([key, value]),
-    ])
-  );
+console.log(Calculator);
+
+declare const Equals: React.FC<{ onClick(): void }>;
+declare const NumPad: React.FC;
+declare const Output: React.FC;
+
+declare function parse(input: string): {
+  left: number;
+  right: number;
+  operation: 'sum';
+};
+
+declare function useUserInput(): string;
+
+function sum(left: Expression, right: Expression) {
+  return left.eval() + right.eval();
 }
 
-function getAllUsers(): Promise<User[]> {
-  return get('api/v1/users');
+function test() {
+  assert(sum(Expression.literal(2), Expression.literal(2)) === 4);
+
+  assert(sum(Expression.literal(2), Expression.literal(0)) === 2);
+
+  assert(sum(Expression.literal(2), Expression.literal(-2)) === 0);
+
+  console.log('PASSES');
 }
 
-// Команда выполняющая сайд-эффект
-function deleteUserById(id: number): Promise<void> {
-  return get(`api/v1/users/${id}/delete`);
-}
-
-// Команда не выполняющая сайд-эффект (уже по сути командой не является)
-function deleteUserByIdMock(id: number): Promise<void> {
-  return ['deleteUserById', [id]];
-}
-
-const getAllUsersRealAfter = (): Promise<{ content: User[] }> =>
-  get('api/v2/users');
-
-const getAllUsersAdapted = () =>
-  getAllUsersRealAfter().then((it) => it.content);
-
-// Реальный внешний API
-declare function createExternalDS(): DataSource;
-
-// Заглушки
-const createMockDS = (): DataSource => ({
-  getUsers: async () => [],
-});
-
-function main() {
-  // Таблица ввода/вывода. Пока рассматриваются только входные данные
-  // интерфейс источников данных DataSource
-  const expected = new Map<DataSource, unknown>([
-    [
-      // Реализация конкретных queries переопределяется следующим образом
-      {
-        ...createMockDS(),
-        getUsers: async () => [{ name: 'Fedor' }, { name: 'Vasiliy' }],
-      },
-      //<editor-fold desc="Скрыто">
-      0,
-      //</editor-fold>
-    ],
-  ]);
-
-  test(expected, program);
-}
-
-function program(source: DataSource): CommandResult {
-  //<editor-fold desc="Много кода">
-  const a = 0;
-  //</editor-fold>
-
-  // Где-то в глубине какого-то обработчика событий
-  source.deleteUserById(10);
-}
-
-type CommandResult = unknown;
-
-declare function expect(input: unknown): any;
+console.log(test());
 
 function wait() {
   return new Promise((resolve) => setTimeout(resolve, 1_000));
 }
-
-type DataSource = {
-  getUsers(): Promise<User[]>;
-  deleteUserById(id: User['id']): Promise<void>;
-};
 
 type User = { name: string; id: number };
 
@@ -114,4 +72,12 @@ declare const List: React.FC<{ of: unknown }>;
 
 declare function get(input: unknown): any;
 
-declare function assert(input: unknown): void;
+function assert(input: boolean) {
+  if (input) {
+    return true;
+  }
+
+  throw new Error('Failed');
+}
+
+export {};
