710479f633e76fb42164dffaf58f7a70f7558a0e
Khaimov <khaimov@reksoft.ru>
2023-12-26 19:12:15 +0300

    Type level Nat arithmetics

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,35 @@
+type Brand<T, TName extends string> = T & { [Key in `__${TName}`]: TName };
 type Either<A, B> = Left<A> | Right<B>;
 type Left<A> = { type: 'left'; value: A };
 type Right<B> = { type: 'right'; value: B };
 
 declare function isEitherLeft<T>(input: Either<T, unknown>): input is Left<T>;
 
-type Matcher<TOrigin, TResult> = {
-  on<TRefined extends TOrigin>(
-    refiner: (input: TOrigin) => input is TRefined,
-    act: (input: TRefined) => TResult,
-  ): Matcher<Exclude<TOrigin, TRefined>, TResult>;
-  orElse(act: (input: TOrigin) => TResult): TResult;
-};
-
-declare function match<TOrigin, TResult>(input: TOrigin): Matcher<TOrigin, TResult>;
-
-// Either empty or not
-type List<T> = Either<void, [T, List<T>]>;
-
-declare function createEmptyList<T>(): List<T>;
-
-declare function followedBy<T>(element: T, origin: List<T>): List<T>;
-
-// Either zero or successor (greater than one) of another positive
-type PositiveNumber = Either<void, () => PositiveNumber>;
-
-declare function zero(): Left<void>;
-
-declare function successorOf(input: PositiveNumber): PositiveNumber;
-
-function fromNumber(input: number): PositiveNumber {
-  if (input === 0) {
-    return zero();
-  }
-
-  return successorOf(fromNumber(input - 1));
-}
-
-const isZero = isEitherLeft<void>;
-
 declare function not(input: boolean): boolean;
 
-const isEven = (input: PositiveNumber): boolean =>
-  match<PositiveNumber, boolean>(input)
-    .on(isZero, () => true)
-    .orElse(({ value }) => not(isEven(value())));
-
-function createPositiveFromLength(input: string): PositiveNumber {
-  return fromNumber(input.length);
-}
-
-const getEachWordLength = (words: List<string>): List<PositiveNumber> =>
-  match<List<string>, List<PositiveNumber>>(words)
-    .on(isEitherLeft, (_) => createEmptyList())
-    .orElse(({ value: [word, words] }) => followedBy(createPositiveFromLength(word), getEachWordLength(words)));
-
 declare function hole<T>(): T;
+
+type Zero = Brand<unknown, 'Zero'>;
+type Successor<R extends PositiveNumber> = () => R;
+type PositiveNumber = Zero | Successor<any>;
+
+type Add<TLeft extends PositiveNumber, TRight extends PositiveNumber> = TLeft extends Zero
+  ? TRight
+  : TLeft extends Successor<infer TOf>
+  ? Add<TOf, Successor<TRight>>
+  : TRight;
+
+type ToNumber<T extends PositiveNumber> = _ToNumber<T>['length'];
+
+type _ToNumber<T extends PositiveNumber> = T extends Zero
+  ? []
+  : T extends Successor<infer TOf>
+  ? [0, ..._ToNumber<TOf>]
+  : [];
+
+type One = Successor<Zero>;
+type Two = Successor<One>;
+
+declare const t: ToNumber<Add<Two, One>>;
 
 export {};
