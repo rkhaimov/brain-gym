cd0f66b9d9aee01dba3d2597d2de5a1c8724ba80
Khaimov <haimovroman12@ya.ru>
2025-03-11 12:33:52 +0300

    factorial

--- main.hs
+++ main.hs
@@ -1,16 +1,10 @@
 module Main (main) where
+import Language.Haskell.TH (prim)
 
-printSmallNumber num =
-  if num < 10
-    then print num
-    else print "the number is too big!"
-
-guardSize num
-  | num < 3 = "that's a small number"
-  | num < 10 = "that's a medium number"
-  | num < 100 = "that's a pretty big number"
-  | num < 1000 = "wow, that's a giant number"
-  | otherwise = "that's an unfathomably big number"
+factorial n =
+  if n == 1
+    then 1
+    else n * factorial (n - 1)
 
 -- run runghc main.hs
-main = printSmallNumber 10
+main = print $ show $ factorial 5
