4ba6e97e1198492787d760e98a78a44beaacce21
Khaimov <haimovroman12@ya.ru>
2025-03-11 12:25:25 +0300

    let where bindings

--- main.hs
+++ main.hs
@@ -1,6 +1,12 @@
-module Main where
+module Main (main) where
 
-makeGreeting = (<>) . (<> " ")
+letWhereGreeting name place =
+  let salutation = "Hello " <> name
+      meetingInfo = location "Tuesday"
+   in let date = 18
+       in salutation <> " " <> meetingInfo <> show date
+  where
+    location day = "we met at " <> place <> " on a " <> day
 
 -- run runghc main.hs
-main = print $ makeGreeting "Hello" "Jeff"
+main = print $ letWhereGreeting "Jeff" "Bridge"
