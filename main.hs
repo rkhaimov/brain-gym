ea1f9f2ede22dca74fd4bd6de8da416cb30eaecf
khaimov <khaimov@reksoft.ru>
2025-05-12 19:02:21 +0300

    range

--- main.hs
+++ main.hs
@@ -1,11 +1,14 @@
 module Main (main) where
+
 import Language.Haskell.TH (prim)
 
-fibonacci n
-    | n == 0 = 0
-    | n == 1 = 1
-    | otherwise = fibonacci (n - 1) + fibonacci (n - 2)
+countdown :: Integer -> [Integer]
+countdown n
+  | n == 0 = [0]
+  | otherwise = n : countdown (n - 1)
+  
+range = reverse . countdown
 
--- 71 page
+-- 73 page
 -- run runghc main.hs
-main = print $ show $ fibonacci 10
+main = print $ show $ range 2
