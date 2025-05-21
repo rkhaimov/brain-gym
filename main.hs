3c575e37720a979ba341fe77d0b22d2087527401
khaimov <khaimov@reksoft.ru>
2025-05-21 20:01:07 +0300

    uncurring

--- main.hs
+++ main.hs
@@ -1,7 +1,6 @@
 module Main (main) where
 
-pairs :: [Int] -> [Int] -> [(Int, Int)]
-pairs as bs = [(a, b) | a <- as, a `elem` bs, b <- bs, odd b]
+pairs = uncurry (+)
 
 -- 87 page
 -- run runghc main.hs
