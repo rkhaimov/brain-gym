4afb84c82cb763fd9b482380126b7e8b5386bca3
Khaimov <haimovroman12@ya.ru>
2025-06-09 21:02:45 +0300

    partial check

--- main.hs
+++ main.hs
@@ -1,29 +1,7 @@
 module Main (main) where
 
-addValues [] = 0
-addValues (first : rest) = first + (addValues rest)
-
-modifyPair ("Hello", _) = "this is a salutation"
-modifyPair (_, "George") = "this is a message for George"
-modifyPair p = "I don't know what " <> show p <> " means"
-
-favoriteFood person =
-  case person of
-    "Ren" -> "Tofu"
-    "Rebecca" -> "Falafel"
-    "George" -> "Banana"
-    name -> "I Don't Know what " <> name <> " likes!"
-
-handleNums l =
-  case l of
-    [] -> "An empty list"
-    [x]
-      | x == 0 -> "a list called: [0]"
-      | x == 1 -> "a singular list of [1]"
-      | even x -> "a singleton list containing an even number"
-      | otherwise -> "the list contains " <> (show x)
-    _list -> "the list has more than 1 element"
+partialFunction 0 = "I only work for 0"
 
 -- 90 page
--- run runghc main.hs
-main = print $ show $ addValues [1, 2, 3]
+-- run runghc -Wincomplete-patterns main.hs
+main = print $ show $ partialFunction 1
