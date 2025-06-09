4d355d803204127a4c3af520b190a6124fa3df79
Khaimov <haimovroman12@ya.ru>
2025-06-09 20:19:16 +0300

    pattern matching

--- main.hs
+++ main.hs
@@ -1,8 +1,29 @@
 module Main (main) where
 
--- id' :: [a] -> a
-id' a = head a
+addValues [] = 0
+addValues (first : rest) = first + (addValues rest)
+
+modifyPair ("Hello", _) = "this is a salutation"
+modifyPair (_, "George") = "this is a message for George"
+modifyPair p = "I don't know what " <> show p <> " means"
+
+favoriteFood person =
+  case person of
+    "Ren" -> "Tofu"
+    "Rebecca" -> "Falafel"
+    "George" -> "Banana"
+    name -> "I Don't Know what " <> name <> " likes!"
+
+handleNums l =
+  case l of
+    [] -> "An empty list"
+    [x]
+      | x == 0 -> "a list called: [0]"
+      | x == 1 -> "a singular list of [1]"
+      | even x -> "a singleton list containing an even number"
+      | otherwise -> "the list contains " <> (show x)
+    _list -> "the list has more than 1 element"
 
 -- 90 page
 -- run runghc main.hs
-main = print $ show $ pairs [1, 2, 3]
+main = print $ show $ addValues [1, 2, 3]
