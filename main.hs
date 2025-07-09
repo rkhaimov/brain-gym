27098891a1cfd422ddf9155e879ecdd8833d0540
Khaimov <haimovroman12@ya.ru>
2025-07-09 20:51:07 +0300

    zipWith hard

--- main.hs
+++ main.hs
@@ -1,14 +1,11 @@
 module Main (main) where
 
-zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
-zipWith' join as bs =
-  if null as
-    then []
-    else
-      let a = head as
-       in let b = head bs
-           in join a b : zipWith' join (tail as) (tail bs)
+zipWith' join = foldr onA onB
+  where
+    onB bs = []
+    onA a onBF [] = []
+    onA a onBF (b : bs) = join a b : onBF bs
 
 -- Creating Lazy Streams 105 page
 -- runghc -Wincomplete-patterns main.hs
-main = print $ show $ zipWith' (+) [1, 2, 3] [1, 2, 3]
+main = print $ show $ zipWith' (,) [1, 2, 3] [4, 5, 6]
