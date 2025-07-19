3ac1f8f9ee7da3c2a900abb6bdea2266bfba8d52
Khaimov <haimovroman12@ya.ru>
2025-07-19 21:10:41 +0300

    types

--- main.hs
+++ main.hs
@@ -4,6 +4,6 @@ concatMap' aToB = foldr mapConcat []
   where
     mapConcat as bs = map aToB as <> bs
 
--- Creating Lazy Streams 110 page
+-- Creating Lazy Streams 122 page
 -- runghc -Wincomplete-patterns main.hs
 main = print $ show $ concatMap' (* 2) [[1, 2], [3, 4]]
