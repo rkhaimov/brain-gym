52ea73a5da65aeff2f0d324b78c1a7da482e37ec
khaimov <khaimov@reksoft.ru>
2025-07-28 12:03:48 +0300

    more types

--- main.hs
+++ main.hs
@@ -3,7 +3,7 @@ module Main (main) where
 n :: Int
 n = _
 
--- Creating Lazy Streams 131 page
+-- Creating Lazy Streams 134 page
 -- runghc -Wincomplete-patterns main.hs
 -- https://github.com/BartoszMilewski/DaoFP/blob/master/DaoFP.pdf
 main = print $ show $ concatMap' (* 2) [[1, 2], [3, 4]]
