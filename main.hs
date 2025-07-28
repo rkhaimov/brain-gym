0e19d861d6fa16860e23c0b3a9da0471c86b3f4f
khaimov <khaimov@reksoft.ru>
2025-07-28 19:55:39 +0300

    custom types

--- main.hs
+++ main.hs
@@ -1,18 +1,13 @@
 module Main (main) where
 
+data CustomerInfo = CustomerInfo
+  { firstName :: String,
+    lastName :: String,
+    widgetCount :: Int,
+    balance :: Int
+  }
 
-data LeftType
-data RightType
-
-onLeft :: LeftType -> Int
-onLeft left = onRight left;
-
-onRight :: RightType -> Int
-onRight right = undefined;
-
---Couldn't match expected type ‘RightType’ with actual type ‘LeftType’
-
--- Creating Lazy Streams 134 page
+-- Creating Lazy Streams 142 page
 -- runghc -Wincomplete-patterns main.hs
 -- https://github.com/BartoszMilewski/DaoFP/blob/master/DaoFP.pdf
 main = print (show undefined)
