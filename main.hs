3b148b2f6eb23cce2cb075e578a5270618404f54
Khaimov <haimovroman12@ya.ru>
2025-08-04 18:36:19 +0300

    peano ns

--- main.hs
+++ main.hs
@@ -1,8 +1,25 @@
 module Main (main) where
 
-data Either a b = Left a | Right b
+data Peano = Zero | Successor Peano
 
--- Creating Lazy Streams 157 page
+toPeano :: Int -> Peano
+toPeano 0 = Zero
+toPeano n = Successor $ toPeano (n - 1)
+
+fromPeano :: Peano -> Int
+fromPeano Zero = 0
+fromPeano (Successor of') = 1 + fromPeano of'
+
+isPeanoEquals :: Peano -> Peano -> Bool
+isPeanoEquals Zero Zero = True
+isPeanoEquals (Successor ls) (Successor rs) = isPeanoEquals ls rs
+isPeanoEquals _ _ = False
+
+addPeano :: Peano -> Peano -> Peano
+addPeano Zero right = right
+addPeano (Successor ls) right = addPeano ls (Successor right)
+
+-- 157 page
 -- runghc -Wincomplete-patterns main.hs
 -- https://github.com/BartoszMilewski/DaoFP/blob/master/DaoFP.pdf
-main = undefined
+main = print $ show $ fromPeano $ addPeano (toPeano 5) (toPeano 10)
