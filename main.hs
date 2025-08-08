3249f089d0cba26f55bf1426d7f11835acb0fdad
Khaimov <haimovroman12@ya.ru>
2025-08-08 16:48:31 +0300

    calculator v0

--- main.hs
+++ main.hs
@@ -1,29 +1,72 @@
 module Main (main) where
 
-data List a = Empty | Cons a (List a)
+import Text.Read (Lexeme (Char, String), readMaybe)
 
-fromList :: [a] -> List a
-fromList = foldr Cons Empty
+data Operation = Add | Sub | Mul
 
-toList :: List a -> [a]
-toList = foldr' (:) []
+apply :: Operation -> (Int -> Int -> Int)
+apply operation =
+  case operation of
+    Add -> (+)
+    Sub -> (-)
+    Mul -> (*)
 
-foldr' :: (a -> b -> b) -> b -> List a -> b
-foldr' _ b Empty = b
-foldr' f b (Cons x xs) = f x (foldr' f b xs)
+data Expression = Expression
+  { initial :: Int,
+    others :: [(Operation, Int)]
+  }
 
-foldl' :: (b -> a -> b) -> b -> List a -> b
-foldl' _ b Empty = b
-foldl' f b (Cons x xs) = foldl' f (f b x) xs
+toInt :: Expression -> Int
+toInt expression =
+  foldr calc (initial expression) (others expression)
+  where
+    calc :: (Operation, Int) -> Int -> Int
+    calc (operation, right) left = apply operation left right
 
-head' :: List a -> Maybe a
-head' Empty = Nothing
-head' (Cons x _) = Just x
+zero :: Expression
+zero =
+  Expression
+    { initial = 0,
+      others = []
+    }
 
-reverse' :: List a -> List a
-reverse' = foldl' (flip Cons) Empty
+eval :: String -> Maybe Int
+eval = fmap toInt . fromString
+  where
+    fromString :: String -> Maybe Expression
+    fromString expression =
+      let parts = words expression
+          minitial = createInitial parts
+          mothers = createOthers (take (length parts - 1) parts)
+       in createExpression minitial mothers
+    createInitial :: [String] -> Maybe Int
+    createInitial [] = Nothing
+    createInitial parts = parseInt $ last parts
+    createOthers :: [String] -> Maybe [(Operation, Int)]
+    createOthers [] = Just []
+    createOthers (operation : coef : tail) =
+      case createOthers tail of
+        Nothing -> Nothing
+        (Just others) -> fmap (: others) (createOperationInt (parseOperation operation) (parseInt coef) :: Maybe (Operation, Int))
+    createOthers _ = Nothing
+    createOperationInt :: Maybe Operation -> Maybe Int -> Maybe (Operation, Int)
+    createOperationInt Nothing _ = Nothing
+    createOperationInt _ Nothing = Nothing
+    createOperationInt (Just operation) (Just int) = Just (operation, int)
+    parseOperation :: String -> Maybe Operation
+    parseOperation char = case char of
+      "+" -> Just Add
+      "-" -> Just Sub
+      "*" -> Just Mul
+      _ -> Nothing
+    createExpression :: Maybe Int -> Maybe [(Operation, Int)] -> Maybe Expression
+    createExpression Nothing _ = Nothing
+    createExpression _ Nothing = Nothing
+    createExpression (Just initial) (Just others) = Just Expression {initial = initial, others = others}
+    parseInt :: String -> Maybe Int
+    parseInt = readMaybe
 
 -- 160 page
 -- runghc -Wincomplete-patterns main.hs
 -- https://github.com/BartoszMilewski/DaoFP/blob/master/DaoFP.pdf
-main = print $ show $ toList $ reverse' $ fromList [1, 2, 3]
+main = print $ show $ eval "* 3 - 2 + 10 + 1 0"
