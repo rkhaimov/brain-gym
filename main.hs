05ea76487f640216731c61d05c31904e76e4e49f
Khaimov <haimovroman12@ya.ru>
2025-08-20 17:23:10 +0300

    implementing type class

--- main.hs
+++ main.hs
@@ -1,6 +1,13 @@
 module Main where
 
--- 229 page
+data Box a = Box
+  { value :: a
+  }
+
+instance Eq (Box Int) where
+  (==) left right = value left == value right
+
+-- 231 page
 -- runghc -Wincomplete-patterns main.hs
 -- https://github.com/BartoszMilewski/DaoFP/blob/master/DaoFP.pdf
 main = undefined
