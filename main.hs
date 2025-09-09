95b23e09649f0dd20199ea68df4ca32215dae784
khaimov <roman.khaimov@reksoft.com>
2025-09-09 20:12:18 +0300

    defining type class

--- main.hs
+++ main.hs
@@ -4,8 +4,16 @@ data Box a = Box
   { value :: a
   }
 
+--Type class definition example
+--class Eq a where
+---- (==) :: a -> a -> Bool
+
 instance Eq (Box Int) where
   (==) left right = value left == value right
+
+unique :: Eq a => [a] -> [a]
+unique [] = []
+unique (a : as) = a : unique (filter (/= a) as)
 
 -- 231 page
 -- runghc -Wincomplete-patterns main.hs
