6866fb16c31665b720e04c5f33a39646d485649f
khaimov <roman.khaimov@reksoft.com>
2026-01-24 19:01:24 +0300

    agent tools

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log('Hello node');
+import { ChatOpenAI } from '@langchain/openai';
+import { createAgent, HumanMessage, tool } from 'langchain';
+import { CONFIGURATION } from './private';
+import z from 'zod';
+
+void main();
+
+async function main() {
+  const getTemperature = tool(
+    ({ city }) => `In ${city} temperature is 21 degrees.`,
+    {
+      name: 'get_temperature',
+      description: 'Get the temperature for a given city',
+      schema: z.object({
+        city: z.string().describe('A city where temperature must be measured'),
+      }),
+    },
+  );
+
+  const agent = createAgent({
+    model: new ChatOpenAI(CONFIGURATION),
+    tools: [getTemperature],
+  });
+
+  console.log(
+    await agent.invoke({
+      messages: [new HumanMessage("What's the temperature in New-York city?")],
+    }),
+  );
+}
