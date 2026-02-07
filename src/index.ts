f749179a70e38863561185f482ac11436c7157d0
khaimov <roman.khaimov@reksoft.com>
2026-02-07 19:49:40 +0300

    tool call

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,41 @@
-import {
-  AIMessage,
-  AIMessageChunk,
-  BaseMessageLike,
-  SystemMessage,
-} from '@langchain/core/messages';
+import { BaseMessageLike } from '@langchain/core/dist/messages/base';
+import { HumanMessage, SystemMessage } from '@langchain/core/messages';
+import { tool } from '@langchain/core/tools';
 import { ChatOpenAI } from '@langchain/openai';
-import { HumanMessage } from 'langchain';
-import { createRL } from './createRL';
+import assert from 'node:assert';
+import { z } from 'zod';
 import { CONNECTION_CONFIG } from './private';
-
-const rl = createRL();
 
 void main();
 
 async function main() {
-  const model = new ChatOpenAI(CONNECTION_CONFIG);
+  const getWeather = tool((input) => `It's sunny in ${input.location} today.`, {
+    name: 'get_weather',
+    description: 'Get the weather at a location.',
+    schema: z.object({
+      location: z.string().describe('The location to get the weather for'),
+    }),
+  });
 
-  await chat(model, [new HumanMessage('Hello')]);
-}
+  const model = new ChatOpenAI(CONNECTION_CONFIG).bindTools([getWeather]);
 
-async function chat(model: ChatOpenAI, messages: BaseMessageLike[]) {
-  const stream = await model.stream(messages);
+  const input = [
+    new SystemMessage(
+      'You are an assistant who provides user with weather info',
+    ),
+    new HumanMessage('What is the weather like in Tokyo?'),
+  ];
 
-  let full = new AIMessageChunk([]);
-  for await (const chunk of stream) {
-    full = full.concat(chunk);
+  const response = await model.invoke(input);
+
+  const calls: BaseMessageLike[] = [];
+  for (const call of response.tool_calls ?? []) {
+    assert(call.name === 'get_weather');
+
+    calls.push(await getWeather.invoke(call));
   }
 
-  const prompt = await rl.ask(full.text);
+  const result = await model.invoke([...input, response, ...calls]);
 
-  console.log('\n');
-
-  return chat(model, [...messages, full, new HumanMessage(prompt)]);
+  console.log(result.text);
 }
