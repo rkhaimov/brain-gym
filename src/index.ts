01852427cd207dd71bf2187a444e6ef82d874ef0
khaimov <roman.khaimov@reksoft.com>
2026-02-07 19:53:38 +0300

    structured output

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,26 @@
-import { BaseMessageLike } from '@langchain/core/dist/messages/base';
-import { HumanMessage, SystemMessage } from '@langchain/core/messages';
-import { tool } from '@langchain/core/tools';
 import { ChatOpenAI } from '@langchain/openai';
-import assert from 'node:assert';
 import { z } from 'zod';
 import { CONNECTION_CONFIG } from './private';
 
 void main();
 
+// https://docs.langchain.com/oss/javascript/langchain/models#structured-output
 async function main() {
-  const getWeather = tool((input) => `It's sunny in ${input.location} today.`, {
-    name: 'get_weather',
-    description: 'Get the weather at a location.',
-    schema: z.object({
-      location: z.string().describe('The location to get the weather for'),
+  const model = new ChatOpenAI(CONNECTION_CONFIG).withStructuredOutput(
+    z.object({
+      title: z.string().describe('The title of the movie'),
+      year: z.number().describe('The year the movie was released'),
+      director: z.string().describe('The director of the movie'),
+      rating: z.int().min(0).max(10).describe("The movie's rating"),
     }),
-  });
+  );
 
-  const model = new ChatOpenAI(CONNECTION_CONFIG).bindTools([getWeather]);
+  const start = performance.now();
+  console.log(
+    await model.invoke('Provide details about the movie Inception', {
+      timeout: 30_000,
+    }),
+  );
 
-  const input = [
-    new SystemMessage(
-      'You are an assistant who provides user with weather info',
-    ),
-    new HumanMessage('What is the weather like in Tokyo?'),
-  ];
-
-  const response = await model.invoke(input);
-
-  const calls: BaseMessageLike[] = [];
-  for (const call of response.tool_calls ?? []) {
-    assert(call.name === 'get_weather');
-
-    calls.push(await getWeather.invoke(call));
-  }
-
-  const result = await model.invoke([...input, response, ...calls]);
-
-  console.log(result.text);
+  console.log(performance.now() - start);
 }
