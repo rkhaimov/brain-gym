ede710ee5eccb8279285b193c8c2dc449155062e
khaimov <roman.khaimov@reksoft.com>
2026-02-18 18:39:59 +0300

    PII detection

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,33 @@
-import { Command, MemorySaver } from '@langchain/langgraph';
+import { MemorySaver } from '@langchain/langgraph';
 import { ChatOpenAI } from '@langchain/openai';
 
-import { createAgent, humanInTheLoopMiddleware, tool } from 'langchain';
-import { z } from 'zod';
+import { createAgent, piiMiddleware } from 'langchain';
 import { CONNECTION_CONFIG } from './private';
 
 void main();
 
-// https://docs.langchain.com/oss/javascript/langchain/middleware/built-in
+// https://docs.langchain.com/oss/javascript/langchain/middleware/built-in#to-do-list
 async function main() {
-  const getWeather = tool(
-    async ({ city }) => {
-      return `The weather in ${city} is always sunny!`;
-    },
-    {
-      name: 'get_weather',
-      description: 'Get weather for a given city.',
-      schema: z.object({
-        city: z.string(),
-      }),
-    },
-  );
-
   const model = new ChatOpenAI(CONNECTION_CONFIG);
 
   const agent = createAgent({
     model: model,
-    tools: [getWeather],
+    tools: [],
     checkpointer: new MemorySaver(),
     middleware: [
-      humanInTheLoopMiddleware({
-        interruptOn: {
-          ['get_weather']: {
-            allowedDecisions: ['approve', 'edit', 'reject'],
-            description: '🚨 weather reading requires administrator approval',
-          },
-        },
-      }),
+      piiMiddleware('email', { strategy: 'redact', applyToInput: true }),
     ],
   });
 
-  await agent.invoke(
+  const result = await agent.invoke(
     {
       messages: [
         {
           role: 'user',
-          content: 'what is the weather in sf',
+          content: 'hello my email is hello@email.com',
         },
       ],
     },
-    { configurable: { thread_id: '1' } },
-  );
-
-  const result = await agent.invoke(
-    new Command({
-      resume: {
-        decisions: [
-          {
-            type: 'reject',
-            message: 'I am sorry, I want to know weather in New York instead',
-          },
-        ],
-      },
-    }),
     { configurable: { thread_id: '1' } },
   );
 
