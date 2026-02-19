f43b0b3a415b054d206e805937b045365eee6a54
khaimov <roman.khaimov@reksoft.com>
2026-02-19 17:38:49 +0300

    tools selector

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,39 @@
+import { Calculator } from '@langchain/community/tools/calculator';
 import { ChatOpenAI } from '@langchain/openai';
 
-import { createAgent, todoListMiddleware } from 'langchain';
+import { createAgent, llmToolSelectorMiddleware, tool } from 'langchain';
+import { z } from 'zod';
 import { CONNECTION_CONFIG } from './private';
-
-const PROMPT = `## \`write_todos\`
-
-You have access to the \`write_todos\` tool to help you manage and plan objectives. 
-Use this tool to ensure that you are tracking each necessary step and giving the user visibility into your progress.
-This tool is very helpful for planning objectives, and for breaking down these objectives into smaller steps.
-
-It is critical that you mark todos as completed as soon as you are done with a step. Do not batch up multiple steps before marking them as completed.
-
-## Important To-Do List Usage Notes to Remember
-- The \`write_todos\` tool should never be called multiple times in parallel.
-- Don't be afraid to revise the To-Do list as you go. New information may reveal new tasks that need to be done, or old tasks that are irrelevant.`;
 
 void main();
 
-// https://docs.langchain.com/oss/javascript/langchain/middleware/built-in#to-do-list
 async function main() {
   const model = new ChatOpenAI(CONNECTION_CONFIG);
 
+  const getWeather = tool(
+    (args) => `The weather in ${args.city} is always sunny!`,
+    {
+      name: 'get_weather',
+      description: 'Get weather for a given city.',
+      schema: z.object({
+        city: z.string(),
+      }),
+    },
+  );
+
+  const calculator = new Calculator();
+
   const agent = createAgent({
     model: model,
-    tools: [],
-    middleware: [
-      todoListMiddleware({
-        systemPrompt: PROMPT,
-      }),
-    ],
+    tools: [calculator, getWeather],
+    middleware: [llmToolSelectorMiddleware({ model: model })],
   });
 
   const result = await agent.invoke({
     messages: [
       {
         role: 'user',
-        content: 'How to cook a cake?',
+        content: 'What is the weather in SF?',
       },
     ],
   });
