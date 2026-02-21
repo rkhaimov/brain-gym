a17d0612d1645a96391651faa140a20e1fbe3fc3
khaimov <roman.khaimov@reksoft.com>
2026-02-21 16:24:01 +0300

    mcp stdio

--- main.js
+++
@@ -1,9 +0,0 @@
-main();
-
-function main() {
-  console.log('2 + 2 is equal to', sum('2', 2));
-}
-
-function sum(a, b) {
-  return a + b;
-}
--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,24 @@
+import { MultiServerMCPClient } from '@langchain/mcp-adapters';
 import { ChatOpenAI } from '@langchain/openai';
-import { createSubAgentMiddleware } from 'deepagents';
+import { createAgent } from 'langchain';
 
-import { createAgent, tool } from 'langchain';
-import { z } from 'zod';
 import { CONNECTION_CONFIG } from './private';
 
 void main();
 
 // https://docs.langchain.com/oss/javascript/langchain/guardrails
 async function main() {
-  const model = new ChatOpenAI(CONNECTION_CONFIG);
-
-  const getWeather = tool(
-    (args) => `The weather in ${args.city} is always sunny!`,
-    {
-      name: 'get_weather',
-      description: 'Get weather for a given city.',
-      schema: z.object({
-        city: z.string(),
-      }),
+  const client = new MultiServerMCPClient({
+    weather: {
+      transport: 'stdio',
+      command: 'npx ts-node ./getWeather.ts',
+      args: [],
     },
-  );
+  });
 
   const agent = createAgent({
-    model: model,
-    middleware: [
-      createSubAgentMiddleware({
-        defaultModel: model,
-        subagents: [
-          {
-            name: 'weather',
-            description: 'This subagent can get weather in cities.',
-            systemPrompt:
-              'Use the get_weather tool to get the weather in a city.',
-            tools: [getWeather],
-            model: 'gpt-4.1',
-            middleware: [],
-          },
-        ],
-      }),
-    ],
+    model: new ChatOpenAI(CONNECTION_CONFIG),
+    tools: await client.getTools(),
   });
 
   const result = await agent.invoke({
