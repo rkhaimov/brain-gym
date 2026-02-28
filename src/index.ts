6e057319ca608867e4a47248501b50c87127d149
khaimov <roman.khaimov@reksoft.com>
2026-02-28 17:16:17 +0300

    deep agents research

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,74 @@
 import { ChatOpenAI } from '@langchain/openai';
+import { TavilySearch } from '@langchain/tavily';
 import { createDeepAgent } from 'deepagents';
 import { createMiddleware, tool } from 'langchain';
 import { z } from 'zod';
-import { CONNECTION_CONFIG } from './private';
+import { CONNECTION_CONFIG, TAVILY_API_KEY } from './private';
 
 // https://docs.langchain.com/oss/javascript/deepagents/overview
 async function main() {
-  const getWeather = tool(({ city }) => `It's always sunny in ${city}!`, {
-    name: 'get_weather',
-    description: 'Get the weather for a given city',
-    schema: z.object({
-      city: z.string(),
-    }),
+  const schema = z.object({
+    query: z.string().describe('The search query'),
+    maxResults: z
+      .number()
+      .optional()
+      .default(5)
+      .describe('Maximum number of results to return'),
+    topic: z
+      .enum(['general', 'news', 'finance'])
+      .optional()
+      .default('general')
+      .describe('Search topic category'),
+    includeRawContent: z
+      .boolean()
+      .optional()
+      .default(false)
+      .describe('Whether to include raw content'),
   });
 
   const agent = createDeepAgent({
     model: new ChatOpenAI(CONNECTION_CONFIG),
-    systemPrompt: 'You are a helpful assistant',
-    tools: [getWeather],
+    systemPrompt: `You are an expert researcher.
+    
+    Your job is to conduct thorough research and then write a polished report.
+    You have access to an internet search tool as your primary means of gathering information.
+
+    ## \`internet_search\`
+
+    Use this to run an internet search for a given query. You can specify the max number of results to return, the topic, and whether raw content should be included.
+    `,
+    tools: [
+      tool(
+        async ({
+          query,
+          maxResults = 5,
+          topic = 'general',
+          includeRawContent = false,
+        }: z.Infer<typeof schema>) => {
+          return new TavilySearch({
+            maxResults,
+            tavilyApiKey: TAVILY_API_KEY,
+            includeRawContent,
+            topic,
+          })._call({ query });
+        },
+        {
+          name: 'internet_search',
+          description: 'Run a web search',
+          schema,
+        },
+      ),
+    ],
     middleware: [
       createMiddleware({
         name: 'debug',
-        wrapModelCall: (request, handler) => {
-          debugger;
+        wrapToolCall: (request, handler) => {
+          console.log(request.toolCall.name);
+          console.log(request.toolCall.args);
 
+          return handler(request);
+        },
+        wrapModelCall: (request, handler) => {
           return handler(request);
         },
       }),
@@ -32,7 +77,12 @@ async function main() {
 
   console.log(
     await agent.invoke({
-      messages: [{ role: 'user', content: "What's the weather in Tokyo?" }],
+      messages: [
+        {
+          role: 'user',
+          content: 'What is langgraph?',
+        },
+      ],
     }),
   );
 }
