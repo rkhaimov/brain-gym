48ea14ba5e6303568a76cb196b8b8bd7845245a4
khaimov <roman.khaimov@reksoft.com>
2026-02-28 19:37:13 +0300

    deep agents skills

--- src/index.ts
+++ src/index.ts
@@ -1,78 +1,54 @@
 import { ChatOpenAI } from '@langchain/openai';
-import { TavilySearch } from '@langchain/tavily';
-import { createDeepAgent } from 'deepagents';
-import { createMiddleware, tool } from 'langchain';
-import { z } from 'zod';
-import { CONNECTION_CONFIG, TAVILY_API_KEY } from './private';
+import {
+  CompositeBackend,
+  createDeepAgent,
+  FileData,
+  StateBackend,
+  StoreBackend,
+} from 'deepagents';
+import { createMiddleware } from 'langchain';
+import { CONNECTION_CONFIG } from './private';
 
 // https://docs.langchain.com/oss/javascript/deepagents/overview
 async function main() {
-  const schema = z.object({
-    query: z.string().describe('The search query'),
-    maxResults: z
-      .number()
-      .optional()
-      .default(5)
-      .describe('Maximum number of results to return'),
-    topic: z
-      .enum(['general', 'news', 'finance'])
-      .optional()
-      .default('general')
-      .describe('Search topic category'),
-    includeRawContent: z
-      .boolean()
-      .optional()
-      .default(false)
-      .describe('Whether to include raw content'),
-  });
+  function createFileData(content: string): FileData {
+    const now = new Date().toISOString();
+    return {
+      content: content.split('\n'),
+      created_at: now,
+      modified_at: now,
+    };
+  }
+
+  const skillsFiles: Record<string, FileData> = {};
+
+  const skillUrl =
+    'https://raw.githubusercontent.com/langchain-ai/deepagentsjs/refs/heads/main/examples/skills/langgraph-docs/SKILL.md';
+  const response = await fetch(skillUrl);
+  const skillContent = await response.text();
+
+  skillsFiles['/skills/langgraph-docs/SKILL.md'] = createFileData(skillContent);
 
   const agent = createDeepAgent({
     model: new ChatOpenAI(CONNECTION_CONFIG),
-    systemPrompt: `You are an expert researcher.
-    
-    Your job is to conduct thorough research and then write a polished report.
-    You have access to an internet search tool as your primary means of gathering information.
-
-    ## \`internet_search\`
-
-    Use this to run an internet search for a given query. You can specify the max number of results to return, the topic, and whether raw content should be included.
-    `,
-    tools: [
-      tool(
-        async ({
-          query,
-          maxResults = 5,
-          topic = 'general',
-          includeRawContent = false,
-        }: z.Infer<typeof schema>) => {
-          return new TavilySearch({
-            maxResults,
-            tavilyApiKey: TAVILY_API_KEY,
-            includeRawContent,
-            topic,
-          })._call({ query });
-        },
-        {
-          name: 'internet_search',
-          description: 'Run a web search',
-          schema,
-        },
-      ),
-    ],
+    systemPrompt: 'You are helpful assistant',
+    tools: [],
     middleware: [
       createMiddleware({
         name: 'debug',
         wrapToolCall: (request, handler) => {
-          console.log(request.toolCall.name);
-          console.log(request.toolCall.args);
+          debugger;
 
           return handler(request);
         },
         wrapModelCall: (request, handler) => {
+          debugger;
+
           return handler(request);
         },
       }),
     ],
+    skills: ['/skills/'],
   });
 
   console.log(
@@ -80,9 +56,11 @@ async function main() {
       messages: [
         {
           role: 'user',
-          content: 'What is langgraph?',
+          content:
+            'what is langraph? Use the langgraph-docs skill if available.',
         },
       ],
+      files: skillsFiles,
     }),
   );
 }
--- tasks.md
+++ tasks.md
@@ -1,9 +1,7 @@
-* Translator for docuzaurus docs
-* ASCII picture generator loop (one model generates -> another model verifies)
-* Learn https://docs.langchain.com/oss/javascript/integrations/tools/webbrowser
-* Debug https://docs.langchain.com/oss/javascript/deepagents/harness
-* Implement own agent system (with HITL, streaming, etc.) based on langchain model providers (or with own fetch functions)
-* Learn https://llmstxt.org
 * Implement https://docs.langchain.com/oss/javascript/langgraph/agentic-rag#overview example using enhanced API
+* Learn https://llmstxt.org
 * Learn https://www.promptingguide.ai
-* Implement TODO with short-memory compression using raw log store -> chunking -> structure extraction -> merging based on user query https://chatgpt.com/s/t_69918f2c5db88191ac900414be9d7449
+* Learn https://agentskills.io/home
+* Learn https://docs.langchain.com/oss/javascript/integrations/tools/webbrowser
+* ASCII picture generator loop (one model generates -> another model verifies)
+* Translator for docuzaurus docs
