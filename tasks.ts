4ee2e0d890de3071bf41c301567fe1347e119901
khaimov <roman.khaimov@reksoft.com>
2026-05-16 22:15:40 +0300

    TaskPool

--- tasks.ts
+++ tasks.ts
@@ -1,37 +1,94 @@
-/**
- * Min heap structure
- */
-type MinHeap = number[];
+class Queue<T> {
+  enqueue(value: T): void {}
 
-function left(i: number): number {
-  return 2 * i + 1;
+  dequeue(): T | undefined {}
+
+  detach(value: T): void {}
+
+  exists(value: T): boolean {}
+
+  get size(): number {}
 }
 
-function right(i: number): number {
-  return 2 * i + 2;
-}
+type PendingTask<T> = {
+  signal: AbortSignal;
+  start: PromiseWithResolvers<void>;
+  finish: PromiseWithResolvers<T>;
+  handle(signal: AbortSignal): Promise<T>;
+};
 
-function p(i: number): number {
-  return Math.floor((i - 1) / 2);
-}
+class TaskPool {
+  private queue = new Queue<PendingTask<unknown>>();
+  private working = 0;
 
-function insert(heap: MinHeap, insertion: number): MinHeap {
-  let index = heap.push(insertion) - 1;
+  constructor(private concurrency: number) {}
 
-  while (index > 0) {
-    const parent = heap.at(p(index));
+  async run<T>(
+    handle: (signal: AbortSignal) => Promise<T>,
+    signal = new AbortController().signal,
+  ): Promise<T> {
+    signal.throwIfAborted();
 
-    if (parent === undefined || parent <= insertion) {
-      break;
+    const pending: PendingTask<unknown> = {
+      handle,
+      signal,
+      start: Promise.withResolvers(),
+      finish: Promise.withResolvers(),
+    };
+
+    if (this.working < this.concurrency) {
+      void this.start(pending);
+    } else {
+      void this.enqueue(pending);
     }
 
-    heap[p(index)] = insertion;
-    heap[index] = parent;
-
-    index = p(index);
+    return pending.finish.promise as Promise<T>;
   }
 
-  return heap;
-}
+  private async enqueue(pending: PendingTask<unknown>) {
+    this.queue.enqueue(pending);
 
-insert([1, 9, 3], 0);
+    const abort = Promise.withResolvers();
+
+    try {
+      pending.signal.addEventListener("abort", abort.resolve);
+
+      const aborted = await Promise.race([
+        pending.start.promise.then(() => false),
+        abort.promise.then(() => true),
+      ]);
+
+      if (aborted) {
+        this.queue.detach(pending);
+
+        pending.finish.reject(pending.signal.reason);
+      }
+    } finally {
+      pending.signal.removeEventListener("abort", abort.resolve);
+    }
+  }
+
+  private async start(pending: PendingTask<unknown>): Promise<void> {
+    pending.start.resolve();
+
+    this.working += 1;
+
+    try {
+      pending.signal.throwIfAborted();
+
+      const result = await pending.handle(pending.signal);
+
+      pending.finish.resolve(result);
+    } catch (error) {
+      pending.finish.reject(error);
+    } finally {
+      this.working -= 1;
+
+      const pending = this.queue.dequeue();
+
+      if (pending) {
+        void this.start(pending);
+      }
+    }
+  }
+}
