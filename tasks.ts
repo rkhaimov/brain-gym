4ac3eff3de68c257349854aafd50c328e12754b1
khaimov <roman.khaimov@reksoft.com>
2026-05-16 10:01:16 +0300

    MinHeap

--- tasks.ts
+++ tasks.ts
@@ -1,79 +1,37 @@
-type QueueElement<T> = {
-  value: T;
-  next: QueueElement<T>;
-};
+/**
+ * Min heap structure
+ */
+type MinHeap = number[];
 
-class Queue<T> {
-  private state: undefined | { head: QueueElement<T>; tail: QueueElement<T> };
-
-  enqueue(value: T): void {
-    if (this.state === undefined) {
-      const element = { value } as QueueElement<T>;
-
-      element.next = element;
-
-      this.state = { head: element, tail: element };
-
-      return;
-    }
-
-    const element: QueueElement<T> = { value, next: this.state.head };
-
-    this.state.tail.next = element;
-    this.state.tail = element;
-  }
-
-  dequeue(): T | undefined {
-    if (this.state === undefined) {
-      return undefined;
-    }
-
-    if (this.state.head === this.state.head.next) {
-      const head = this.state.head;
-
-      this.state = undefined;
-
-      return head.value;
-    }
-
-    const head = this.state.head;
-
-    this.state.head = head.next;
-    this.state.tail.next = head.next;
-
-    return head.value;
-  }
-
-  toArray() {
-    const elements: T[] = [];
-
-    if (this.state === undefined) {
-      return elements;
-    }
-
-    let element = this.state.head;
-
-    while (true) {
-      if (elements.length > 0 && element === this.state.head) {
-        return elements;
-      }
-
-      elements.push(element.value);
-
-      element = element.next;
-    }
-  }
+function left(i: number): number {
+  return 2 * i + 1;
 }
 
-const list = new Queue<number>();
+function right(i: number): number {
+  return 2 * i + 2;
+}
 
-list.enqueue(1);
-list.enqueue(2);
-list.enqueue(3);
-list.enqueue(4);
+function p(i: number): number {
+  return Math.floor((i - 1) / 2);
+}
 
-console.log(list.toArray()); // [ 4, 3, 2, 1 ]
+function insert(heap: MinHeap, insertion: number): MinHeap {
+  let index = heap.push(insertion) - 1;
 
-list.dequeue();
+  while (index > 0) {
+    const parent = heap.at(p(index));
 
-console.log(list.toArray()); // [ 3, 4, 2, 1 ]
+    if (parent === undefined || parent <= insertion) {
+      break;
+    }
+
+    heap[p(index)] = insertion;
+    heap[index] = parent;
+
+    index = p(index);
+  }
+
+  return heap;
+}
+
+insert([1, 9, 3], 0);
