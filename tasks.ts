a97a1cea3fb52b3f0fc4a936e90f677782b7aaec
khaimov <roman.khaimov@reksoft.com>
2026-05-17 15:54:51 +0300

    Trie

--- tasks.ts
+++ tasks.ts
@@ -1,54 +1,101 @@
-class EventEmitter<TEvents extends Record<string, unknown>> {
-  // `any` is on purpose here to avoid unnecessary type-casts and maintain strong public interface
-  private listeners = new Map<keyof TEvents, Set<(arg: any) => void>>();
+// tree
+// graph
+// trie
+// bloom filter
+// segment tree
+type TrieNode = {
+  children: Map<string, TrieNode>;
+  isWord: boolean;
+};
 
-  on<TEvent extends keyof TEvents>(
-    event: TEvent,
-    listener: (value: TEvents[TEvent]) => void,
-  ) {
-    const listeners = this.listeners.get(event);
+const ROOT = { isWord: false, children: new Map() };
 
-    if (listeners === undefined) {
-      this.listeners.set(event, new Set([listener]));
-    } else {
-      listeners.add(listener);
-    }
-  }
+const t0 = insert(ROOT, "cat");
+const t1 = insert(t0, "cate");
+const t2 = remove(t1, "cat");
 
-  off<TEvent extends keyof TEvents>(
-    event: TEvent,
-    listener: (value: TEvents[TEvent]) => void,
-  ) {
-    this.listeners.get(event)?.delete(listener);
-  }
+console.log(search(t2!, "cate")); // outputs true
+console.log(search(t2!, "cat")); // outputs false
 
-  once<TEvent extends keyof TEvents>(
-    event: TEvent,
-    listener: (value: TEvents[TEvent]) => void,
-  ) {
-    const once = (data: TEvents[TEvent]) => {
-      try {
-        listener(data);
-      } finally {
-        this.off(event, once);
-      }
-    };
+function remove(root: TrieNode, word: string): TrieNode | undefined {
+  const char = word.at(0);
 
-    this.on(event, once);
-  }
-
-  emit<TEvent extends keyof TEvents>(
-    event: TEvent,
-    arg: TEvents[TEvent],
-  ): void {
-    const listeners = this.listeners.get(event);
-
-    if (listeners === undefined) {
-      return;
+  if (char === undefined) {
+    if (root.children.size === 0) {
+      return undefined;
     }
 
-    for (const listener of listeners) {
-      listener(arg);
-    }
+    root.isWord = false;
+
+    return root;
   }
+
+  const child = root.children.get(char);
+
+  if (child === undefined) {
+    return root;
+  }
+
+  const updated = remove(child, word.slice(1));
+
+  if (updated === undefined) {
+    root.children.delete(char);
+  }
+
+  if (root.children.size === 0 && !root.isWord) {
+    return undefined;
+  }
+
+  return root;
+}
+
+function insert(root: TrieNode, word: string): TrieNode {
+  let curr: TrieNode = root;
+
+  for (const char of word) {
+    const child = curr.children.get(char);
+
+    if (child) {
+      curr = child;
+
+      continue;
+    }
+
+    const node: TrieNode = { children: new Map(), isWord: false };
+
+    curr.children.set(char, node);
+
+    curr = node;
+  }
+
+  curr.isWord = true;
+
+  return root;
+}
+
+function search(root: TrieNode, word: string): boolean {
+  return _findNodeByPrefix(root, word)?.isWord ?? false;
+}
+
+function startsWith(root: TrieNode, prefix: string): boolean {
+  return _findNodeByPrefix(root, prefix) !== undefined;
+}
+
+function _findNodeByPrefix(
+  root: TrieNode,
+  prefix: string,
+): TrieNode | undefined {
+  let curr: TrieNode = root;
+
+  for (const char of prefix) {
+    const child = curr.children.get(char);
+
+    if (child === undefined) {
+      return undefined;
+    }
+
+    curr = child;
+  }
+
+  return curr;
 }
